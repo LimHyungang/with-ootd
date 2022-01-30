232af069818fc37d83c5b522a1c3c8be545db2ca
wooyongjeong <wooyong.dev@gmail.com>
2022-01-30 11:08:27 +0900

    First Commit

---
+++ src/main/java/com/ootd/with/WithOotdApplication.java
@@ -0,0 +1,13 @@
+package com.ootd.with;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithOotdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithOotdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ootd/with/WithOotdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ootd.with;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithOotdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
