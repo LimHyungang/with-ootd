385ee8140c387a3f70b39140d95b50c1fa6b2b85
LimHyungang <hyungang7@gmail.com>
2022-02-23 16:38:42 +0900

    [220223-LHG] - 불필요한 import 제거

--- src/main/java/com/ootd/with/domain/hashtag/Hashtag.java
+++ src/main/java/com/ootd/with/domain/hashtag/Hashtag.java
@@ -1,15 +1,11 @@
 package com.ootd.with.domain.hashtag;
 
 import com.ootd.with.domain.BaseTimeEntity;
-import com.ootd.with.domain.post.Post;
-import com.ootd.with.domain.post.PostHashtag;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor
